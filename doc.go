ab0d45cc4a33d49c5b6f1f5eeda3486f0460c97f
Alan Strohm <alan.strohm@gmail.com>
2013-02-19 19:51:48 -0500

    Project description.

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+go-gramps is a library for interacting with GRAMPs. It is a work in progress.
+
+The library is currently focused on GRAMPs XML (.gramps) files. It allows them
+to be parsed and exposes Go structs representing the files.  The structs can
+then be modified and serialized back into a valid file.
+*/
+package gogramps
