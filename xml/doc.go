fbb7f0dd8fdce6c251129e7b00c74fb930c2300e
Alan Strohm <alan.strohm@gmail.com>
2013-02-19 19:57:46 -0500

    xml package description.

---
+++ xml/doc.go
@@ -0,0 +1,6 @@
+/*
+Package xml provides structs. for representing a GRAMPs XML file. It also
+provides functions to read a GRAMPs XML file into a tree of these structs or
+serialize the structs back out to a file.
+*/
+package xml
