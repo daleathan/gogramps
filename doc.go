cf89e2b94c51ea41174125d0609f4d849c399316
Alan Strohm <alan.strohm@gmail.com>
2013-02-22 23:22:36 -0500

    Doc tweaks.

--- doc.go
+++ doc.go
@@ -1,8 +1,11 @@
 /*
-gogramps is a library for interacting with GRAMPs. It is a work in progress.
+gogramps is a library for interacting with Gramps, an open-source genealogical
+program. It is a work in progress.
 
-The library is currently focused on GRAMPs XML (.gramps) files. It allows them
+The library is currently focused on Gramps XML (.gramps) files. It allows them
 to be parsed and exposes Go structs representing the files.  The structs can
 then be modified and serialized back into a valid file.
+
+For more information on Gramps, visit gramps-project.org.
 */
 package gogramps
